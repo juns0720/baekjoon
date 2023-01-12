f54ff1310a82974365fbe0d62522526152d20cd0
junseo Lee <74139368+juns0720@users.noreply.github.com>
2023-01-12 22:36:27 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,a,b;
+    a = 0;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n+1; i++)
+    {
+        a += i;
+      
+
+    }
+
+    printf("%d", a);
+
+
+
+    return 0;
+}

2155aac8622d680e727d7455c5c942eb05c6fa99
junseo Lee <74139368+juns0720@users.noreply.github.com>
2023-01-12 22:37:44 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, x;
+    int answer;
+    
+
+    while (scanf("%d %d", &n, &x)!=EOF)
+    {
+       
+            printf("%d\n", n + x);
+    }
+
+
+ 
+
+    return 0;
+}

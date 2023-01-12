7c7f710a465d6221799de2e05c8f44f608fb7e41
junseo Lee <74139368+juns0720@users.noreply.github.com>
2023-01-12 22:36:52 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 344 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,a,b;
+    a = 0;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a + b);
+      
+
+    }
+
+
+
+    return 0;
+}

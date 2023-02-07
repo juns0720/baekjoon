cb9e00099bf3947dc3f36ca669c23922d53c3175
junseo Lee <74139368+juns0720@users.noreply.github.com>
2023-02-07 15:39:59 +0900

    [Silver IV] Title: 큐 2, Time: 1396 ms, Memory: 258784 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+queue = deque([])
+n = int(input())
+
+for i in range(n):
+    str = input().split()
+    if str[0] == 'push':
+        queue.append(str[1])
+    elif str[0] == 'pop':
+        if len(queue) == 0:
+            print('-1')
+        else:
+            t = queue.popleft()
+            print(t)
+    elif str[0] == 'size':
+        print(len(queue))
+    elif str[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif str[0] == 'front':
+        if len(queue) == 0:
+            print('-1')
+        else:
+            print(queue[0])
+    elif str[0] == 'back':
+        if len(queue) == 0:
+            print('-1')       
+        else:
+            print(queue[-1]) 
+

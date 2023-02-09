98693c69c5c0b128ec23b0195312241a3f0b34a9
junseo Lee <74139368+juns0720@users.noreply.github.com>
2023-02-10 01:01:38 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 148 ms, Memory: 115592 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,24 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+queue = []      #사람수 7, 3번째 제거
+n = list(map(int,input().split()))
+
+l = [str(i) for i in range(1,(n[0])+1)]
+count = n[1]-1
+while True:
+
+    if len(l) == 0:
+        break
+    else:
+        if count < len(l):
+            queue.append(l[count])
+            l.pop(count)
+            count+=(n[1]-1)            
+        elif count >= len(l):
+             count-=len(l)
+    if len(queue) == n[0]:
+        break
+
+print("<",', '.join(queue),">", sep="")

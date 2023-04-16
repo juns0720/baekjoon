7c9db35af09d4a06ee0ada43b9e410a3c74aa9ba
junseo Lee <74139368+juns0720@users.noreply.github.com>
2023-04-16 17:41:05 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -4,6 +4,7 @@ input = sys.stdin.readline
 L = int(input())
 s = list(input().strip())
 c_s = []
+M = 1234567891
 for index in s:
     c_s.append(ord(index)-96)
 i = 0
@@ -11,4 +12,4 @@ Hash = 0
 for apb in c_s:
     Hash+=apb*(31**i)
     i+=1
-print(Hash)
+print(Hash%M)

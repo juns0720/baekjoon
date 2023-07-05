da072f072a77a1c7a63ff60a8d65bd6b78cee050
junseo Lee <74139368+juns0720@users.noreply.github.com>
2023-07-05 13:25:16 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,27 +1,22 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-N = int(input())
-M = int(input())
-graph = [[] for _ in range(N+1)]
-virus = []
-for _ in range(M):
-    node,num = map(int,input().split())
-    graph[node].append(num)
-    graph[num].append(node)
+n = int(input())
+m = int(input())
+graph = [[] for _ in range(n+1)]
+visited = [False]*(n+1)
+for _ in range(m):
+    x,y = map(int,input().split())
+    graph[x].append(y)
+    graph[y].append(x)
 
-visited = [False for _ in range(N+1)]
-def BFS(graph, start, visited):
-    queue = deque([])
-    queue.append(start)
-    visited[start] = True
-    while queue:
-        v = queue.popleft()
-        for i in graph[v]:
-            if not visited[i]:
-                virus.append(i)
-                queue.append(i)
-                BFS(graph,i,visited)
-BFS(graph, 1, visited)
-print(len(virus))
+def DFS(t,count):
+	node = t
+	visited[node] = True
+	count+=1
+	
+	for i in graph[node]:
+		if visited[i] == False:
+			count = DFS(i,count)
+	return count
+print(DFS(1,-1))

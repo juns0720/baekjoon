4dd18e4b4cd5341fc80c020ba8c94da9625c201f
junseo Lee <74139368+juns0720@users.noreply.github.com>
2023-07-15 17:46:23 +0900

    [Gold IV] Title: N-Queen, Time: 9776 ms, Memory: 133412 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+
+def pm_pop():
+    plus.pop()
+    minus.pop()
+def pm_append(cnt,i):
+    plus.append(cnt+int(i))
+    minus.append(cnt-int(i))
+
+def cal(temp,cnt):
+    if 'x' not in num:
+        num[-1]+=1
+        pm_pop()
+        return
+    else:
+        for i in range(1,N+1):
+            if num[i-1] =='x':
+                if cnt+int(i) not in plus and cnt-int(i) not in minus:    
+                    pm_append(cnt,i)
+                    num[i-1] = 'o'
+                    cal(temp+str(i),cnt+1)
+                    num[i-1] = 'x'
+        if cnt > 2:
+           pm_pop()
+
+
+N = int(input())
+num = ['x' for _ in range(N)]
+num.append(0)
+
+
+for i in range(1,N+1):
+    temp = ''
+    plus = [1+i]
+    minus = [1-i]
+    num[i-1] = 'o'
+    cal(temp+str(i),2)
+    num[i-1] = 'x'
+print(num[-1])

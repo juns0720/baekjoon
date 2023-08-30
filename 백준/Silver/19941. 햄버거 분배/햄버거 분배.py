1fd7cbb2776a1c3b34462bbe5b4178f9ee5067a2
junseo Lee <74139368+juns0720@users.noreply.github.com>
2023-08-30 12:24:23 +0900

    [Silver III] Title: 햄버거 분배, Time: 188 ms, Memory: 117388 KB -BaekjoonHub

---
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N,K = map(int,input().split())
+arr = list(input().strip())
+
+def cal(x):
+    Min = x-K
+    Max = x+K
+    if Min < 0:
+        Min = 0
+    if Max > N-1:
+        Max = N-1
+    for i in range(Min,x):
+        if arr[i] == 'H':
+            arr[i] = 'X'
+            return 1
+    for i in range(x,Max+1):
+        if arr[i] == 'H':
+            arr[i] = 'X'
+            return 1
+    return 0
+
+Sum = 0
+for i in range(len(arr)):
+    if arr[i] == 'P':
+        Sum+=cal(i)
+print(Sum)

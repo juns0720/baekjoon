1ba1a9453115b7fa2d47a23899d6f6fdaaa2f5e2
junseo Lee <74139368+juns0720@users.noreply.github.com>
2023-09-22 17:53:16 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 172 ms, Memory: 122688 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N,K = map(int,input().split())
+graph = [abs(K-N) for _ in range((K+N)*2+1)]
+visited = [0 for _ in range((K+N)*2+1)]
+graph[N] = 0
+queue = deque([N])
+def BFS():
+    while queue:
+        n = queue.popleft()
+        visited[n] = 1
+        dn = [n,-1,1]
+        for i in range(3):
+            tn = n+dn[i]
+            if tn > -1 and tn < K*2+1 and not visited[tn]:
+                queue.append(tn)
+                visited[tn] = 1
+                if i > 0:
+                    graph[tn] = min(graph[n]+1,graph[tn])
+                else:
+                    graph[tn] = min(graph[tn],graph[n])
+    print(graph[K])
+BFS()

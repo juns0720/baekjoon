ee88bfcd702a3707682c6cf740d937cc96710682
junseo Lee <74139368+juns0720@users.noreply.github.com>
2024-02-04 23:37:37 +0900

    [Gold V] Title: 신기한 소수, Time: 120 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -6,43 +6,29 @@ public class Main {
     public static BufferedReader br;
     public static int[] FirstIndexes = {2, 3, 5, 7};
     public static int[] OtherIndexes = {1, 3, 7, 9};
-    public static int number;
+    public static int maxDigit;
+
 
 
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        for (int firstIndex : FirstIndexes) {
-            number = firstIndex;
-            if (N == 1)
-                System.out.println(number);
-            else
-                makeNumber(number, N);
-        }
-
+        maxDigit = Integer.parseInt(br.readLine());
+        for (int firstIndex : FirstIndexes)
+                makeNumber(firstIndex, 1);
     }
 
-    public static void makeNumber(int number, int maxDigit){
+    public static void makeNumber(int number, int digit){
         if (isPrime(number) == false)
             return;
-        if (split(number) == maxDigit){
+
+        if (digit == maxDigit){
             System.out.println(number);
             return;
         }
-        for (int otherIndex : OtherIndexes) {
+        for (int otherIndex : OtherIndexes)
+            makeNumber(number*10 +otherIndex, digit+1);
+    }
 
-            makeNumber(number*10 +otherIndex, maxDigit);
-        }
-    }
-    public static int split(int number){
-        int n = 0;
-        while(number != 0){
-            number /= 10;
-            n+=1;
-        }
-        return n;
-    }
     public static boolean isPrime(int number){
 
         if (number == 1)

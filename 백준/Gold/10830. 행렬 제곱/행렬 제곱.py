b881bca524776549328c1bbff7f765b5702e1990
junseo Lee <74139368+juns0720@users.noreply.github.com>
2024-05-02 17:50:06 +0900

    [Gold IV] Title: 행렬 제곱, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.py
@@ -0,0 +1,60 @@
+import sys
+input = sys.stdin.readline
+
+
+def cal_odd():
+    ##현재 좌표
+    tmp_list = [[0 for _ in range(n)] for _ in range(n)]
+    for row in range(n):
+        for col in range(n):
+            tmp = 0
+            for i in range(n):
+                    tmp+=result[row][i]*matrix[i][col]
+            tmp_list[row][col] = tmp % 1000
+    for row in range(n):
+        for col in range(n):
+            result[row][col] = tmp_list[row][col]
+
+def cal_even():
+    
+    tmp_list = [[0 for _ in range(n)] for _ in range(n)]
+    for row in range(n):
+        for col in range(n):
+            tmp = 0
+            for i in range(n):
+                    tmp+=result[row][i]*result[i][col]
+            tmp_list[row][col] = tmp % 1000
+    for row in range(n):
+        for col in range(n):
+            result[row][col] = tmp_list[row][col]
+
+
+def devide(depth):
+    if depth == 1:
+        return
+    else:
+        devide(depth//2)
+        cal_even()
+        if depth % 2 == 1:
+            cal_odd()
+
+   
+
+     
+
+n,b = map(int,input().split())
+matrix = []
+result = [[0 for _ in range(n)] for _ in range(n)]
+
+    
+for i in range(n):
+    temp_list = list(map(int,input().split()))
+    matrix.append(temp_list)
+    result[i][i] = 1
+
+cal_odd()
+devide(b)
+for row in result:
+    for col in row:
+        print(col,end=' ')
+    print()

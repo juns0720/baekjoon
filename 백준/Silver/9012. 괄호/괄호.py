0433f8add663bce1b9781cb48cbbf1ebfea44c25
junseo Lee <74139368+juns0720@users.noreply.github.com>
2024-06-21 17:11:06 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,47 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-def pop(Stack,t):
-
-    if len(Stack) == 0:
-        return -1
-    elif len(Stack) < 2:
-        return
-    else:       
-        del Stack[t]
-        del Stack[t-1]
-   
-    if len(Stack) >= 2:
-        for i in range(1,(len(Stack))):
-            if i < len(Stack):
-                if Stack[i-1] == '(':
-                    if Stack[i] == ')':
-                        pop(Stack,i)
-    elif len(Stack) < 2:
-        return
-   
-    
-def Vps(list):
-   if len(list) >= 2:
-        for i in range(1,(len(list))):
-            if i < len(list):
-                if list[i-1] == '(':
-                    if list[i] == ')':
-                        pop(list,i)
-
-   if len(list) >= 1:
-       print('NO')
-   elif len(list) == 0:
-       print('YES')
-
-
-
-
-
 n = int(input())
 
 for i in range(n):
-    str1 = []
-    a = input().rstrip()
-    str1 = list(a)
-    Vps(str1)
+    cnt = 0
+    s = list(input().strip())
+    for i in range(len(s)):
+        s1 = s.pop()
+        if s1 == ')':
+            cnt+=1
+        elif s1 == '(':
+            cnt-=1
+            if cnt < 0:
+                break
+
+    print("YES" if cnt == 0 else "NO")
+    

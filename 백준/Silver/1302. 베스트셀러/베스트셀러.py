6ce247ed6fde0925d16d1beb72c4db2f8281eb30
junseo Lee <74139368+juns0720@users.noreply.github.com>
2024-06-21 19:05:30 +0900

    [Silver IV] Title: 베스트셀러, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.py
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -1,9 +1,14 @@
 import sys
-from collections import Counter
+
 input = sys.stdin.readline
 n = int(input())
 
-arr = [input().strip() for _ in range(n)]
-arr.sort()
-items = Counter(arr)
-print(items.most_common(n=1)[0][0])
+dict = dict()
+for i in range(n):
+    s1 = input().strip()
+    if s1 not in dict:
+        dict[s1] = 1
+    else:
+        dict[s1]+=1
+res = list(sorted(dict.items(), key = lambda x: (-x[1],x[0])))
+print(res[0][0])

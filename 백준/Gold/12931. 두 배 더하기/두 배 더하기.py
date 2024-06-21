7282f03b60f69da10897e2f644fd739367498be3
junseo Lee <74139368+juns0720@users.noreply.github.com>
2024-06-21 16:16:39 +0900

    [Gold V] Title: 두 배 더하기, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Gold/12931. 두 배 더하기/두 배 더하기.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+lstB = list(map(int,input().split()))
+lstB.sort(reverse = True)
+cnt = 0
+
+while lstB[0] > 0:
+    for i in range(n):
+        if lstB[i] % 2 == 1:
+            lstB[i]-=1
+            cnt+=1
+    if lstB[0] == 0:
+        break
+    for i in range(n):
+        lstB[i] //= 2
+    cnt+=1
+for i in range(n):
+    cnt+=lstB[i]
+print(cnt)

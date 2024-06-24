19de0ddaf0fb32e5c56e55f7efcecfb7dd88fde7
junseo Lee <74139368+juns0720@users.noreply.github.com>
2024-06-24 13:53:16 +0900

    [Silver III] Title: N과 M (1), Time: 176 ms, Memory: 112364 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,15 +1,17 @@
-N,M = map(int,input().split())
+import sys
+input = sys.stdin.readline
 
-stack = [i for i in range(1,N+1)]
-temp = ''
-def cal(N,M,temp):
-    if len(temp) == M:
-        print(temp.replace('',' ').lstrip())
+N,M = map(int,input().split())
+arr = []
+
+def recursion(cnt, start):
+    if cnt == M:
+        print(' '.join(arr))
         return
     for i in range(1,N+1):
-        if str(i) not in temp:
-            cal(N,M,temp+str(i))
-
-cal(N,M,temp)
-
-        
+        if str(i) not in arr:
+            arr.append(str(i))
+            recursion(cnt+1, start+1)
+            arr.pop()
+            
+recursion(0,1)

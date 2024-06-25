55bcc481b981f5da263b1e53a4e59be1fe4fe410
junseo Lee <74139368+juns0720@users.noreply.github.com>
2024-06-25 20:27:21 +0900

    [Silver II] Title: DFS와 BFS, Time: 176 ms, Memory: 114320 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -2,37 +2,55 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-N,M,V = map(int,input().split())
-graph = [[] for i in range(N+1)]
-visited_DFS, visited_BFS = [False] * (N+1), [False] * (N+1)
-res1, res2 = '', ''
-queue = deque([])
-for _ in range(M):
-    x,y = map(int,input().split())
-    graph[x].append(y)
-    graph[y].append(x)
+n,m,v = map(int,input().split())
 
-def DFS(node,res1):
-    visited_DFS[node] = True
-    res1 += str(node)+' '
+graph = [[] for _ in range(n+1)]
+res_dfs = []
+res_bfs = []
+# 양방향 그래프이므로 v1 -> v2, v2 -> v1 연결
+for _ in range(m):
+    v1,v2 = map(int,input().split())
+    graph[v1].append(v2)
+    graph[v2].append(v1)
 
-    for i in sorted(graph[node]):
-        if visited_DFS[i] != True:
-            res1 = DFS(i,res1)
+# 다른 정점을 방문할 때 정점 번호가 작은 것을 먼저 방문하므로 오름차순으로 정렬
+for v1 in range(1,n+1):
+    graph[v1].sort()
 
-    return res1
 
-def BFS(node,res2):
-    visited_BFS[node] = True
-    res2 +=str(node)+' '
+def dfs(v1):
+    visited[v1] = 1
+    res_dfs.append(v1)
 
-    for i in sorted(graph[node]):
-        if visited_BFS[i] != True:
-            if i not in queue:
-                queue.append(i)
-    if len(queue) != 0:
-        res2 = BFS(queue.popleft(),res2) 
-    return res2
+    for v2 in graph[v1]:
+        if not visited[v2]:
+            dfs(v2)
 
-print(DFS(V,res1))
-print(BFS(V,res2))
+
+def bfs(start_node):
+    queue = deque([])
+    visited = [0 for _ in range(n+1)]
+
+    visited[start_node] = 1
+    res_bfs.append(start_node)
+    queue.append(start_node)
+
+
+    while queue:
+        v1 = queue.popleft()
+        
+        for v2 in graph[v1]:
+            if not visited[v2]:
+                visited[v2] = 1
+                res_bfs.append(v2)
+                queue.append(v2)
+
+
+visited = [0 for _ in range(n+1)]
+dfs(v)
+
+bfs(v)
+
+# 언패킹 -> 리스트 풀어줌
+print(*res_dfs)
+print(*res_bfs)

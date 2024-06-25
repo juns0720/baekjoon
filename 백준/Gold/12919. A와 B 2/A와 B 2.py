b0978288c76b5675ba12c68a7698c64a9a2b8ef5
junseo Lee <74139368+juns0720@users.noreply.github.com>
2024-06-25 17:07:55 +0900

    [Gold V] Title: A와 B 2, Time: 120 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.py
+++ 백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -1,24 +1,8 @@
-"""
-1. 문자열의 뒤에 A를 추가한다.
-2. 문자열의 뒤에 B를 추가하고 문자열을 뒤집는다.
-
-
-A -> BA -> BAB -> BABA
-
-반대로 돌아가기
-BABA -> BAB (뒤에 A 제거)
-BAB -> BA (앞에 B 삭제 후 뒤집기)
-BA -> A (앞에 B 삭제 후 뒤집기)
-
-1. 뒤에 A를 삭제한다.
-2. 앞에 B를 삭제 후 뒤집는다.
-"""
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 def recursion(t):
-    if S == list(t):
+    if S == t:
         print(1)
         exit()
 
@@ -31,7 +15,7 @@ def recursion(t):
         recursion(t[1:][::-1])
 
 
-S = list(input().strip())
-T = list(input().strip())
+S = input().strip()
+T = input().strip()
 recursion(T)
 print(0)

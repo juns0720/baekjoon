7e705900e2f50734ac6675679f4bbea69dbbeff9
junseo Lee <74139368+juns0720@users.noreply.github.com>
2024-06-27 14:49:36 +0900

    [Silver II] Title: 너구리 구구, Time: 152 ms, Memory: 1394040 KB -BaekjoonHub

--- 백준/Silver/18126. 너구리 구구/너구리 구구.py
+++ 백준/Silver/18126. 너구리 구구/너구리 구구.py
@@ -1,31 +1,28 @@
 import sys
-from collections import deque
-input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
 
 n = int(input())
-
-
 graph = [[] for _ in range(n+1)]
-
-queue = deque([])
-visited = [0 for _ in range(n+1)]
-
-def bfs():
-    max_dist = -1
-    while queue:
-        v1, dist= queue.popleft()
-        max_dist = max(dist, max_dist)
-        for v2,cost in graph[v1]:
-            if not visited[v2]:
-                visited[v2] = 1
-                queue.append((v2, dist+cost))
-    return max_dist
-
+visited = [False for _ in range(n+1)]
+result = 0
 
 for i in range(n-1):
-    a,b,c = map(int,input().split())
-    graph[a].append((b,c))
-    graph[b].append((a,c))
-queue.append((1,0))
-visited[1] = 1
-print(bfs())
+    v1,v2,cost = map(int,input().split())
+    #v1 -> v2 일 때의 cost
+    graph[v1].append((v2, cost))
+        #v2 -> v1 일 때의 cost
+    graph[v2].append((v1, cost))
+
+
+def dfs(current_node, current_dist):
+    global result
+    visited[current_node] = True
+    result = max(result, current_dist)
+    for next_node, dist in graph[current_node]:
+        if not visited[next_node]:
+            dfs(next_node, current_dist + dist)
+
+    
+dfs(1,0)
+print(result)
+

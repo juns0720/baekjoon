95de5f398f58a988412f8c06b9f581ca4466983f
junseo Lee <74139368+juns0720@users.noreply.github.com>
2024-07-04 23:19:58 +0900

    [Silver II] Title: 나무 자르기, Time: 476 ms, Memory: 258028 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,23 +1,30 @@
 import sys
 input = sys.stdin.readline
 
+
 n,m = map(int,input().split())
-tree = list(map(int,input().split()))
+trees = list(map(int,input().split()))
 
-start = 1
-end = max(tree)
-while start <= end:
-    mid = (start+end) //2
-    sum = 0
-    for i in tree:
-        if i-mid <=0:
-            continue
-        else:
-            sum+=(i-mid)
-    if sum >= m:
-        start = mid+1
+def cal_height(height):
+    sum_height = 0
+    for tree in trees:
+        cur_height = tree - height
+        if cur_height > 0:
+            sum_height += cur_height
+    return sum_height
+
+s = 0
+e = max(trees)
+
+while s < e:
+    mid = (s + e)//2
+    cur_height = cal_height(mid)
+    if cur_height < m:
+        e = mid
+    elif cur_height == m:
+        break
     else:
-        end = mid-1
-print(end)
-        
-
+        if s == mid:
+            break
+        s = mid
+print(mid)

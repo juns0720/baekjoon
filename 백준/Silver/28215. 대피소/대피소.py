6fb7a58cd4cc16554f1a838570720cfeed460bee
junseo Lee <74139368+juns0720@users.noreply.github.com>
2024-07-12 14:14:23 +0900

    [Silver IV] Title: 대피소, Time: 256 ms, Memory: 112848 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/28215. 대피소/대피소.py
+++ 백준/Silver/28215. 대피소/대피소.py
@@ -1,16 +1,17 @@
 import sys
+import math
 from itertools import combinations
 input = sys.stdin.readline
 
 N,K = map(int,input().split())
 house = []
-
+INF = math.inf
 for i in range(N):
     x,y = map(int,input().split())
     house.append([x,y])
 
-def cal_dist(x1,y1, x2,y2):
-    return abs(x1-x2) + abs(y1-y2)
+def cal_dist(cur_house, shelter):
+    return abs(house[cur_house][0]-house[shelter][0]) + abs(house[cur_house][1]-house[shelter][1])
     
 '''
 1. 전체 집들중 대피소가 될 집 K개를 선택 한다.
@@ -20,24 +21,17 @@ def cal_dist(x1,y1, x2,y2):
 3. 채택된 거리들 중 가장 긴 거리를 선택한다.
 '''
 
-max_dist = 10**20
+max_dist = INF
 
-for shelter in list(combinations([i for i in range(N)],K)): # 대피소 K개를 선택
-    not_shelter = []
-    cur_dist = []
-
+for shelters in list(combinations(range(N),K)): # 대피소 K개를 선택
+    cur_dist = -1
     # 대피소가 아닌 집들 확인
-    for i in range(N):
-        if i not in shelter:
-            not_shelter.append(i)
+
  # 집 별 대피소와의 거리를 계산한다.
-    for i in not_shelter:
-        cur_house_dist = []
-        for j in shelter:
-        #집과 대피소와의 거리를 저장한다.
-            cur_house_dist.append(cal_dist(house[i][0],house[i][1],house[j][0],house[j][1]))
-        #가장 짧은 거리를 cur_dist에 넣는다.
-        cur_dist.append(min(cur_house_dist))
-    
-    max_dist = min(max_dist, max(cur_dist))
+    for cur_house in range(N):
+        cur_house_dist = INF
+        for shelter in shelters:
+            cur_house_dist = min(cur_house_dist, cal_dist(cur_house, shelter))
+        cur_dist = max(cur_dist, cur_house_dist)
+    max_dist = min(max_dist, cur_dist)
 print(max_dist)

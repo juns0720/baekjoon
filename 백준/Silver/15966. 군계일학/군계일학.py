43ce39c64d6305c032d17c5e35966532f9bcb21f
junseo Lee <74139368+juns0720@users.noreply.github.com>
2024-09-07 16:44:44 +0900

    [Silver I] Title: 군계일학, Time: 120 ms, Memory: 132556 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15966. 군계일학/군계일학.py
+++ 백준/Silver/15966. 군계일학/군계일학.py
@@ -2,11 +2,9 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-dp = [1 for _ in range(N+1)]
-arr = list(map(int,input().split()))
-
-for i in range(N):
-    for j in range(i+1, N):
-        if arr[j] == arr[i]+1:
-            dp[j] = max(dp[j], dp[i]+1)
+dp = [0 for _ in range(10**6+1)]
+arr = [0]+list(map(int,input().split()))
+for i in range(1,N+1):
+    num = arr[i]
+    dp[num] = max(dp[num], dp[num-1]+1)
 print(max(dp))

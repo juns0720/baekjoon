f7616fd27c224a2796eaacd002f9844b135b10ed
junseo Lee <74139368+juns0720@users.noreply.github.com>
2024-09-08 03:29:13 +0900

    [Gold IV] Title: 사이클 게임, Time: 872 ms, Memory: 225672 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+parent = [i for i in range(N)]
+pos = [list(map(int,input().split())) for _ in range(M)]
+
+cnt, res = 0,0
+
+def find(v):
+    if parent[v] == v:
+        return v
+    parent[v] = find(parent[v])
+    return parent[v]
+
+def union(v1,v2):
+    v1 = find(v1)
+    v2 = find(v2)
+    if v1 > v2:
+        parent[v1] = v2
+    else:
+        parent[v2] = v1
+
+for v1,v2 in pos:
+    cnt+=1
+    if find(v1) != find(v2):
+        union(v1,v2)
+    else:
+        res = cnt
+        break
+print(res) 

b50c17404f61cf91962a1eab21eb279b561e2b1b
junseo Lee <74139368+juns0720@users.noreply.github.com>
2024-09-24 23:50:59 +0900

    [Silver V] Title: 사이 나쁜 여왕들, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/10469. 사이 나쁜 여왕들/사이 나쁜 여왕들.py
+++ 백준/Silver/10469. 사이 나쁜 여왕들/사이 나쁜 여왕들.py
@@ -1,38 +1,32 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
-
 
 board = [list(input().strip()) for _ in range(8)]
 
-# 증가하는 방향
+# 증가하는 방향만 이동
 dy = [0,1,1,1]
 dx = [1,0,1,-1]
 cnt = 0
-def check(row,col):
+
+def check(y,x):
 
    for d in range(4):
-      queue = deque()
-      queue.append((row,col))
+      ny,nx = y,x
       while True:
-         y,x = queue.popleft()
-         
-         ny = y+dy[d]
-         nx = x+dx[d]
+         ny += dy[d]
+         nx += dx[d]
          if ny < 0 or ny > 7 or nx < 0 or nx > 7:
             break
          if board[ny][nx] == '*':
             print("invalid")
             exit()
 
-         queue.append((ny,nx))
-
 
 
 for row in range(8):
    for col in range(8):
       if board[row][col] == '*':
-         check(row,col)
-         cnt+=1
+        check(row,col)
+        cnt+=1
 
 print("valid") if cnt == 8 else print("invalid")

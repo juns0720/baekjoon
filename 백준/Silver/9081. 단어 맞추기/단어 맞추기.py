4aaf2481154870aa151602e9979101a650de0943
junseo Lee <74139368+juns0720@users.noreply.github.com>
2024-11-26 22:07:15 +0900

    [Silver I] Title: 단어 맞추기, Time: 88 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/9081. 단어 맞추기/단어 맞추기.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+for i in range(T):
+    s = list(input().strip())
+    for i in range(len(s)-2,-1,-1):
+        res = s
+        tmp = []
+        for j in range(i+1, len(s)):
+            if s[j] > s[i]:
+                tmp.append((s[j],j))
+        if tmp:
+            idx = sorted(tmp)[0][1]
+            s[i], s[idx] = s[idx], s[i]
+            res = s[:i+1]+sorted(s[i+1:])
+            break
+    print(''.join(res))

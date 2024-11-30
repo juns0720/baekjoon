37273f9986a6f66c799377cae9018c3c1c51dc42
junseo Lee <74139368+juns0720@users.noreply.github.com>
2024-11-30 23:47:43 +0900

    [Gold II] Title: 문제집, Time: 208 ms, Memory: 116060 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.py
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -6,7 +6,7 @@ n,m = map(int,input().split())
 
 degree = [0 for _ in range(n+1)]
 graph = [[] for _ in range(n+1)]
-visited = [0 for _ in range(n+1)]
+
 
 hq = []
 for i in range(m):
@@ -17,13 +17,14 @@ for i in range(m):
 for gph in graph:
     gph.sort()
 
-for i in range(n):
-    for v1 in range(1,n+1):
-        if degree[v1] == 0 and not visited[v1]:
-            visited[v1] = 1
-            heapq.heappush(hq,v1)
-            for v2 in graph[v1]:
-                degree[v2]-=1
-            break
-    while hq:
-        print(heapq.heappop(hq), end = ' ')
+for v1 in range(1,n+1):
+    if degree[v1] == 0:
+        heapq.heappush(hq,v1)
+
+while hq:
+    v1 = heapq.heappop(hq)
+    print(v1, end = ' ')
+    for v2 in graph[v1]:
+        degree[v2]-=1
+        if degree[v2] == 0:
+            heapq.heappush(hq, v2)

c28962702207bca50988982a76560f686a6b6564
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-01-03 16:58:14 +0900

    [Silver III] Title: 피보나치는 지겨웡~, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/17175. 피보나치는 지겨웡～/피보나치는 지겨웡～.py
+++ 백준/Silver/17175. 피보나치는 지겨웡～/피보나치는 지겨웡～.py
@@ -5,10 +5,8 @@ n = int(input())
 
 dp = [0 for _ in range(n+3)]
 
-dp[0] = 1
-dp[1] = 1
-dp[2] = 3
+dp[0], dp[1] = 1,1
 
-for i in range(3,n+1):
-    dp[i] = dp[i-1] + dp[i-2] + 1
-print(dp[n] % 1000000007)
+for i in range(2,n+1):
+    dp[i] = (dp[i-1] + dp[i-2] + 1)  % 1000000007
+print(dp[n])

79115415cc8446246eb34117f4f14af9e01fb43a
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-01-06 13:50:49 +0900

    [Silver II] Title: 유기농 배추, Time: 128 ms, Memory: 114324 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -2,28 +2,32 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
+
+def BFS():
+
+    while queue:
+        y,x = queue.popleft()
+        board[y][x] = 0
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if nx > -1 and nx < M and ny > -1 and ny < N and board[ny][nx] != 0:
+                if board[ny][nx] == 1:
+                    board[ny][nx] = 0
+                    queue.append((ny,nx))
+
 T = int(input())
 for i in range(T):
     queue = deque([])
     M,N,K = map(int,input().split())
-    board = [[0 for _ in range(M)]for _ in range(N)]
+    board = [[0 for _ in range(M)] for _ in range(N)]
     cnt = 0
     for _ in range(K):
         x,y = map(int,input().split())
         board[y][x] = 1
-    def BFS():
-        dx = [1,0,-1,0]
-        dy = [0,1,0,-1]
-        while queue:
-            y,x = queue.popleft()
-            board[y][x] = 0
-            for i in range(4):
-                nx = x+dx[i]
-                ny = y+dy[i]
-                if nx > -1 and nx < M and ny > -1 and ny < N and board[ny][nx] != 0:
-                    if board[ny][nx] == 1:
-                        board[ny][nx] = 0
-                        queue.append((ny,nx))
+
     for y in range(N):
         for x in range(M):
             if board[y][x] == 1:

8f8b96cdffc9be90d01fac57765829b9c6c06dea
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-01-30 21:39:47 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 268 ms, Memory: 310816 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 n = int(input())
 graph = [[] for _ in range(n+1)]
 visited = [0 for _ in range(n+1)]
-parent = [0 for _ in range(n+1)]
+
 for i in range(n-1):
     v1,v2 = map(int,input().split())
     graph[v1].append(v2)
@@ -14,12 +14,10 @@ for i in range(n-1):
 def dfs(v1):
     for v2 in graph[v1]:
         if not visited[v2]:
-            visited[v2] = 1
-            parent[v2] = v1
+            visited[v2] = v1
             dfs(v2)
 
-visited[1] = 1
 dfs(1)
 
-for v in parent[2:]:
+for v in visited[2:]:
     print(v)

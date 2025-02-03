2b24bcb116318b74719ff41b87616ed3f61ba463
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-02-04 00:36:45 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -2,11 +2,10 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-dp = [[0,0,0,0,0,0,0,0,0,0] for _ in range(N+2)]
+dp = [[0 for _ in range(10)] for _ in range(N+2)]
 dp[1] = [0,1,1,1,1,1,1,1,1,1]
-dp[2] = [1,1,2,2,2,2,2,2,2,1]
 
-for i in range(3,N+1):
+for i in range(2,N+1):
     dp[i][0] = dp[i-1][1]
     dp[i][9] = dp[i-1][8]
     for j in range(1,9):

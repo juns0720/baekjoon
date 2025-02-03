67283577cb68e526870ff3da9182e6c05324461d
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-02-03 23:22:17 +0900

    [Silver II] Title: K번째 소수, Time: 5600 ms, Memory: 362840 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15965. K번째 소수/K번째 소수.py
+++ 백준/Silver/15965. K번째 소수/K번째 소수.py
@@ -1,12 +1,13 @@
 import sys
+import math
 input = sys.stdin.readline
 
-MAX = int(1e7)
+MAX = int(1e6)*8
 
 def make_prime():
     arr = [i for i in range(MAX)]
     arr[1] = 0
-    for i in range(2, MAX):
+    for i in range(2, math.isqrt(MAX)):
         for j in range(i*2, MAX, i):
             arr[j] = 0
     primes = []

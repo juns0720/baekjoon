c0da12872f252da872f7f7c19c238f24b1ed518f
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-02-12 00:26:12 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 268 ms, Memory: 130736 KB -BaekjoonHub

--- 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
@@ -1,27 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-n,m = map(int,input().split())
+n, m = map(int, input().split())
 titles = []
 
-
 def binary_search(power):
-    s = 0
-    e = len(titles)
-    res = 0
-    while s <= e:
+    s, e = 0, len(titles)  # 반열린구간 사용 (e는 포함하지 않음)
+    while s + 1 < e:  # s + 1 < e를 유지하여 반열린구간 방식 적용
         mid = (s + e) // 2
-        if  power <= titles[mid][1]:
-            e = mid - 1
-            res = mid
+        if power <= titles[mid][1]:
+            e = mid  # e를 mid로 변경하여 반열린구간 유지
         else:
-            s = mid + 1
-    print(titles[res][0])
+            s = mid
+    print(titles[s][0] if power <= titles[s][1] else titles[e][0])
 
-for i in range(n):
-    a,b = list(input().strip().split())
-    titles.append([a,int(b)])
+for _ in range(n):
+    a, b = input().strip().split()
+    titles.append([a, int(b)])
 
-for i in range(m):
+for _ in range(m):
     power = int(input())
     binary_search(power)

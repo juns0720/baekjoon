aecf16736d91cbc1617de11e73798d52a89f66f6
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-02-16 16:25:14 +0900

    [Silver II] Title: 피보나치 인버스, Time: 548 ms, Memory: 495856 KB -BaekjoonHub

--- 백준/Silver/10425. 피보나치 인버스/피보나치 인버스.py
+++ 백준/Silver/10425. 피보나치 인버스/피보나치 인버스.py
@@ -2,13 +2,12 @@ import sys
 input = sys.stdin.readline
 
 fibo = [0,1,1,2,3]
-MAX = (10**21000)
-num = 0
-while num <= MAX:
+
+N = 5
+while N <= 100000:
     num = fibo[-1] + fibo[-2]
     fibo.append(num)
-
-
+    N+=1
 
 for tc in range(int(input())):
     n = int(input())

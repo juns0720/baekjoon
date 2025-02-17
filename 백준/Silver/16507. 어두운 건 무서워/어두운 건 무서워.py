ddf1ee49082d6760fa7a0b468bc35d3040460471
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-02-17 21:07:21 +0900

    [Silver I] Title: 어두운 건 무서워, Time: 224 ms, Memory: 128856 KB -BaekjoonHub

--- 백준/Silver/16507. 어두운 건 무서워/어두운 건 무서워.py
+++ 백준/Silver/16507. 어두운 건 무서워/어두운 건 무서워.py
@@ -4,22 +4,16 @@ input = sys.stdin.readline
 r,c,q = map(int,input().split())
 board = [list(map(int,input().split())) for _ in range(r)]
 
-prefix = [[0 for _ in range(c)] for _ in range(r)]
+prefix = [[0 for _ in range(c+1)] for _ in range(r+1)]
 
-for i in range(r):
-    prefix[i][0] = board[i][0]
 
-for i in range(r):
-    for j in range(1,c):
-        prefix[i][j] = prefix[i][j-1]+board[i][j]
+for i in range(1,r+1):
+    for j in range(1,c+1):
+        prefix[i][j] = board[i-1][j-1] + prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1]
 
 for i in range(q):
     sy,sx,ey,ex = map(int,input().split())
-    sy,ey = min(sy-1,ey-1), max(sy-1,ey-1)
-    sx,ex = min(sx-1,ex-1), max(sx-1,ex-1)
-
-    res = 0
-    for j in range(sy,ey+1):
-        res+= prefix[j][ex] if sx == 0 else (prefix[j][ex]-prefix[j][sx-1])
     total = (ey-sy+1) * (ex-sx+1)
+    res = prefix[ey][ex] - prefix[sy-1][ex] - prefix[ey][sx-1] + prefix[sy-1][sx-1]
+
     print(res//total)

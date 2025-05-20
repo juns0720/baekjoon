c68c704505e5c9b8e7ba6c0ee743aa986cfe98d4
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-05-21 03:39:19 +0900

    [Gold III] Title: 거울 설치, Time: 120 ms, Memory: 113500 KB -BaekjoonHub

--- 백준/Gold/2151. 거울 설치/거울 설치.py
+++ 백준/Gold/2151. 거울 설치/거울 설치.py
@@ -1,70 +1,42 @@
 import sys
-import heapq
+from collections import deque
 input = sys.stdin.readline
-INF = sys.maxsize
 d = [(0,1), (1,0), (-1,0), (0,-1)]
 
-N = int(input())
-mirrors = []
-door = []
-board = []
+n = int(input())
 
-for r in range(N):
+board = []
+for r in range(n):
     tmp = list(input().strip())
-    for c in range(N):
-        if tmp[c] == '!':
-            mirrors.append((r,c))
-        elif tmp[c] == '#':
-            door.append((r,c))
+    for c in range(n):
+        if tmp[c] == '#':
+            sy,sx = r,c
+            break
     board.append(tmp)
 
-n = len(mirrors) + 2
-graph = [[] for _ in range(n+1)]
-nodes = dict()
-node = 2
-nodes[door[0]] = 1
-nodes[door[1]] = n
-for mirror in mirrors:
-    nodes[mirror] = node
-    node += 1
-
-#그래프 생성
-def search(y,x):
+def search(sy,sx):
+    queue = deque([(sy,sx, 0)])
+    visited = [[0 for _ in range(n)] for _ in range(n)]
+    visited[sy][sx] = 1
+    while queue:
+        y,x,cost = queue.popleft()
 
         for i in range(4):
             for j in range(1,n):
                 ny = y + d[i][0] * j
                 nx = x + d[i][1] * j
-                if ny < 0 or ny > N-1 or nx < 0 or nx > N-1:
-                    break
-                if board[ny][nx] == '*':
-                    break
-                if board[ny][nx] == '!' or board[ny][nx] == '#':
-                    v1 = nodes[(y,x)]
-                    v2 = nodes[(ny,nx)]
-                    graph[v1].append(v2)
-for i in range(2):
-    search(door[i][0],door[i][1])
-for i in range(n-2):
-    search(mirrors[i][0],mirrors[i][1])
 
-#다익스트라로 최단 거리 탐색
-dist = [INF for _ in range(n+1)]
-dist[1] = 0
-hq = []
-heapq.heappush(hq,(0,1))
+                if ny < 0 or ny > n-1 or nx < 0 or nx > n-1 or board[ny][nx] == '*':
+                    break
+                if visited[ny][nx]:
+                    continue
 
-while hq:
-    cost,v1 = heapq.heappop(hq)
-    if dist[v1] < cost:
-        continue
-    
-    for v2 in graph[v1]:
-        if cost + 1 < dist[v2]:
-            dist[v2] = cost + 1
-            heapq.heappush(hq,(cost+1,v2))
-res = dist[-1]
-if res == INF:
-    print(0)
-else:
-    print(res-1)
+                if board[ny][nx] == '!':
+                    queue.append((ny,nx,cost+1))
+                    visited[ny][nx] = 1
+                elif board[ny][nx] == '#':
+                    return cost
+                
+    return cost
+res = search(sy,sx)
+print(res)

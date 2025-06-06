a9bfe3961ac4f75c8168e8efd3e45e4d755c95ee
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-06-06 13:43:01 +0900

    [Bronze V] Title: 개수 세기, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,9 +1,2 @@
-
-N = int(input())
-a = list(map(int,input().split()))
-cnt = 0
-t = int(input())
-for i in range(N):
-    if a[i] == t:
-        cnt+=1
-print(cnt)
+n = int(input())
+print(list(map(int,input().split())).count(int(input())))

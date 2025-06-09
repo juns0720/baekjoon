9e8c4c977a6f9b40dc93177289c2e234276dddff
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-06-09 23:00:06 +0900

    [Bronze I] Title: 시침과 분침, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/5675. 시침과 분침/시침과 분침.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    try:
+        n = int(input())
+        if n % 6 == 0:
+            print("Y")
+        else:
+            print("N")
+    except:
+        exit()

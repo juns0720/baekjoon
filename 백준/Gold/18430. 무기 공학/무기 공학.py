59e3438fb86764972e669577d3eaaa48c9c69742
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-07-13 17:32:35 +0900

    [Gold IV] Title: 무기 공학, Time: 280 ms, Memory: 112532 KB -BaekjoonHub

--- 백준/Gold/18430. 무기 공학/무기 공학.py
+++ 백준/Gold/18430. 무기 공학/무기 공학.py
@@ -11,7 +11,6 @@ if n*m <= 4:
     print(0)
     exit()
 
-
 def check(s,w):
     global res
     res = max(res,w)
@@ -36,20 +35,5 @@ def check(s,w):
 
 res = 0
 visited = [[0 for _ in range(m)] for _ in range(n)]
-for i in range(n*m):
-    y,x = i//m,i%m
-    visited[y][x] = 1
-    for j in range(4):
-            ny1,nx1 = y + dy[j], x + dx[j]
-            ny2,nx2 = y + dy[(j+1)%4], x + dx[(j+1)%4]
-            if ny1 < 0 or ny1 > n-1 or nx1 < 0 or nx1 > m-1:
-                continue
-            if ny2 < 0 or ny2 > n-1 or nx2 < 0 or nx2 > m-1:
-                continue
-            visited[ny1][nx1] = 1
-            visited[ny2][nx2] = 1
-            check(i+1,board[y][x]*2+board[ny1][nx1]+board[ny2][nx2])
-            visited[ny1][nx1] = 0
-            visited[ny2][nx2] = 0
-    visited[y][x] = 0
+check(0,0)
 print(res)

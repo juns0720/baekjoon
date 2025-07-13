da199d36bb2b1b761e3b48c3e73745a87f4fc12c
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-07-13 15:34:56 +0900

    [Gold IV] Title: 회사 문화 1, Time: 244 ms, Memory: 136216 KB -BaekjoonHub

---
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+arr = list(map(int,input().split()))
+graph = [[] for _ in range(n+1)]
+for i in range(1,n):
+    graph[arr[i]].append(i+1)
+cnt = [0 for _ in range(n+1)]
+
+for _ in range(m):
+    i,w = map(int,input().split())
+    cnt[i]+=w
+queue = deque([(1,0)])
+visited = [0 for _ in range(n+1)]
+res = [0 for _ in range(n+1)]
+while queue:
+    v1,cost = queue.popleft()
+    res[v1] = cost
+    for v2 in graph[v1]:
+        if visited[v2]:
+            continue
+        queue.append((v2,cost+cnt[v2]))
+
+print(*res[1:])

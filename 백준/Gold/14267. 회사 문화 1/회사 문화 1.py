f6d77af2a3a60411c78b666f1822fa5db8ada45d
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-07-13 15:35:41 +0900

    [Gold IV] Title: 회사 문화 1, Time: 196 ms, Memory: 134084 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
@@ -13,14 +13,11 @@ for _ in range(m):
     i,w = map(int,input().split())
     cnt[i]+=w
 queue = deque([(1,0)])
-visited = [0 for _ in range(n+1)]
 res = [0 for _ in range(n+1)]
 while queue:
     v1,cost = queue.popleft()
     res[v1] = cost
     for v2 in graph[v1]:
-        if visited[v2]:
-            continue
         queue.append((v2,cost+cnt[v2]))
 
 print(*res[1:])

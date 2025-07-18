d3ac9da05af9e4ac79a08c612a250369dc5564f5
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-07-19 00:38:20 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 184 ms, Memory: 176176 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
@@ -1,32 +1,26 @@
 import sys
-from math import isqrt
 input = sys.stdin.readline
 
-dic = dict()
 def find():
     cnt = 0
-    for i in arr:
+    for i in range(2,e+1):
+        if i == 1:
+            continue
         j = (mi // i**2)*i**2
         if j < mi:
             j += i**2
         while j < ma+1:
+            if j < mi or j > ma:
+                continue
             if j not in dic:
                 cnt += 1
             dic[j] = 0
             j += i**2
+        
     return cnt
 
 mi,ma = map(int,input().split())
-s, e = isqrt(mi), isqrt(ma)
 
-prime = [i for i in range(e+1)]
-prime[1] = 0
-for i in range(2,isqrt(e+1)):
-    for j in range(i**2,(e+1),i):
-        prime[j] = 0
-arr = []
-for i in prime[2:]:
-    if i:
-        arr.append(i)
-
+s, e = int(mi**0.5), int(ma**0.5)
+dic = dict()
 print(ma-mi+1-find())

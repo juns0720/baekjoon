f5458577c12559450dda0324b450b80fa30dbbf9
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-08-06 13:14:17 +0900

    [Gold IV] Title: 가장 먼 곳, Time: 1160 ms, Memory: 178560 KB -BaekjoonHub

--- 백준/Gold/22865. 가장 먼 곳/가장 먼 곳.py
+++ 백준/Gold/22865. 가장 먼 곳/가장 먼 곳.py
@@ -14,40 +14,33 @@ for i in range(M):
     graph[v2].append((v1,cost))
 
 res = []
-def dijkstra(s,i):
-
-    hq = []
-    heapq.heappush(hq,(0,s))
-    dist[i][s] = 0
+def dijkstra():
 
     while hq:
         cost,v1 = heapq.heappop(hq)
-        if dist[i][v1] < cost:
+        if dist[v1] < cost:
             continue
         for v2,curr_cost in graph[v1]:
-            if dist[i][v2] < cost + curr_cost:
+            if dist[v2] < cost + curr_cost:
                 continue
-            dist[i][v2] = cost+curr_cost
-            heapq.heappush(hq,(dist[i][v2],v2))
+            dist[v2] = cost+curr_cost
+            heapq.heappush(hq,(dist[v2],v2))
 
 
 
-dist = [[INF for _ in range(N+1)] for _ in range(3)]
+dist = [INF for _ in range(N+1)]
+hq = []
 for i in range(3):
-    dijkstra(target[i],i)
+    heapq.heappush(hq,(0,target[i]))
+    dist[target[i]] = 0
+dijkstra()
 
 max_dist = 0
 v1 = 0
-for v2 in range(1,N+1):
-    if v2 in target:
+for v2,curr_dist in enumerate(dist[1:]):
+    if v2+1 in target:
         continue
-    curr_dist = INF
-
-    for j in range(3):
-        curr_dist = min(dist[j][v2], curr_dist)
-
-    if max_dist < curr_dist:
+    if curr_dist > max_dist:
         max_dist = curr_dist
-        v1 = v2
-
+        v1 = v2+1
 print(v1)

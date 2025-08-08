cf1ae5b9c553cce9a010d213619d8f069481a84b
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-08-08 17:02:59 +0900

    [Silver V] Title: 또 수열 문제야, Time: 100 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/31229. 또 수열 문제야/또 수열 문제야.py
+++ 백준/Silver/31229. 또 수열 문제야/또 수열 문제야.py
@@ -1,17 +1,5 @@
 import sys
 input = sys.stdin.readline
-
 N = int(input())
-res = [1]
-for i in range(N-1):
-    for j in range(res[i]+1,10**9+1):
-        can = 0
-        for k in res:
-            if k*j % (k+j) == 0:
-                break
-        else:
-            can = 1
-        if can:
-            res.append(j)
-            break
-print(*res)
+for i in range(N):
+    print(2*i+1, end = ' ')

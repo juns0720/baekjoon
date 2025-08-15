276290cf9e46497441d95d0a97364ee715b1c14a
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-08-15 15:59:18 +0900

    [Silver I] Title: 두 박스, Time: 88 ms, Memory: 108384 KB, Score: 33 point -BaekjoonHub

--- 백준/Silver/15973. 두 박스/두 박스.py
+++ 백준/Silver/15973. 두 박스/두 박스.py
@@ -1,28 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-N = 1000
-board = [[0 for _ in range(N)] for _ in range(N)]
-
+px = []
+py = []
 for i in range(2):
     y1,x1,y2,x2 = map(int,input().split())
-    for r in range(y1,y2+1):
-        for c in range(x1,x2+1):
-            board[r][c] += 1
+    py.append([y2,y1])
+    px.append([x2,x1])
+py.sort(key = lambda x: (-x[0],-x[1]))
+px.sort(key = lambda x: (-x[0],-x[1]))
 
-
-py = set()
-px = set()    
-for r in range(N):
-    for c in range(N):
-        if board[r][c] == 2:
-            py.add(r)
-            px.add(c)
-if len(py) == 1 and len(px) == 1:
+if py[0][1] == py[1][0] and px[0][1] == px[1][0]:
     print("POINT")
-elif len(py) == 1 or len(px) == 1:
+elif py[0][1] == py[1][0] or px[0][1] == px[1][0]:
     print("LINE")
-elif len(py) > 2 and len(px) > 2:
-    print("FACE")
-else:
+elif not (py[0][1] <= py[1][0] <= py[0][0]) or not (px[0][1] <= px[1][0] <= px[0][0]):
     print("NULL")
+else:
+    print("FACE")

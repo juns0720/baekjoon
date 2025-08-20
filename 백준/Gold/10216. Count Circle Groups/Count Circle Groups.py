216549fa458a179a147b02fff5e6522a048a3ae9
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-08-20 11:40:30 +0900

    [Gold IV] Title: Count Circle Groups, Time: 5428 ms, Memory: 133180 KB -BaekjoonHub

---
+++ 백준/Gold/10216. Count Circle Groups/Count Circle Groups.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+
+def union(a,b):
+    a = find(a)
+    b = find(b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+def find(a):
+    if a != parent[a]:
+        parent[a] = find(parent[a])
+    return parent[a]
+
+def isTrue(c1,c2):
+    return ((c1[0]-c2[0])**2 + (c1[1]-c2[1])**2) <= (c1[2] + c2[2])**2
+
+for tc in range(int(input())):
+
+    n = int(input())
+    circle = [list(map(int,input().split())) for _ in range(n)]
+    parent = [i for i in range(n)]
+
+    for i in range(n):
+        for j in range(i+1,n):
+            if find(i) == find(j):
+                continue 
+
+            if isTrue(circle[i],circle[j]):
+                union(i,j)
+
+    res = set()
+    for i in range(n):
+        res.add(find(i))
+    print(len(res))

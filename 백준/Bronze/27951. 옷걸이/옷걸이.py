ae89c6fe6cd7786eba1da5cb644543003e942b0b
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-11-06 13:53:38 +0900

    [Bronze I] Title: 옷걸이, Time: 312 ms, Memory: 221900 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27951. 옷걸이/옷걸이.py
+++ 백준/Bronze/27951. 옷걸이/옷걸이.py
@@ -2,31 +2,34 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-dic = {1: 0, 2: 0, 3: 0}
-lst = list(map(int,input().split()))
+lst = list(map(int, input().split()))
+u, d = map(int, input().split())
 
-for i in lst:
-    dic[i] += 1
+cnt1 = lst.count(1)
+cnt2 = lst.count(2)
+cnt3 = n - cnt1 - cnt2
+
+# 가능성 판정
+if cnt1 > u or cnt2 > d:
+    print("NO")
+    exit()
+
+nu = u - cnt1 
+nd = d - cnt2  
 
 res = []
-u,d = map(int,input().split())
-
 for i in lst:
-    if i == 1 and dic[i] > 0:
+    if i == 1:
         res.append('U')
-        u -= 1
-    elif i == 2 and dic[i] > 0:
+    elif i == 2:
         res.append('D')
-        d -= 1
-    elif i == 3:
-        if u  > 0:
+    else:  
+        if nu > 0:
             res.append('U')
-            u -= 1
-        elif d > 0:
-            res.append("D")
-            d -= 1
-if u < 0 or d < 0:
-    print("NO")
-else:
-    print("YES")
-    print(''.join(res))
+            nu -= 1
+        else:
+            res.append('D')
+            nd -= 1
+
+print("YES")
+print(''.join(res))

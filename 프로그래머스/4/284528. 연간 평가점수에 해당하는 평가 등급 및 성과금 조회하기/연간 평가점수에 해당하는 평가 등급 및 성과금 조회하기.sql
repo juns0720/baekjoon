e5927364f19e74ae79763edb869ce2c309a9b798
junseo Lee <74139368+juns0720@users.noreply.github.com>
2026-03-03 22:50:56 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,26 @@
+SELECT 
+    A.EMP_NO,
+    A.EMP_NAME,
+    B.GRADE,
+    CASE
+        WHEN B.GRADE = 'S' THEN SAL*0.2
+        WHEN B.GRADE = 'A' THEN SAL*0.15
+        WHEN B.GRADE = 'B' THEN SAL*0.1
+        ELSE 0
+        END AS BONUS
+FROM HR_EMPLOYEES A
+JOIN (SELECT
+    A.EMP_NO,
+    A.EMP_NAME,
+    CASE 
+        WHEN AVG(SCORE) >= 96 THEN 'S'
+        WHEN AVG(SCORE) >= 90 THEN 'A'
+        WHEN AVG(SCORE) >= 80 THEN 'B'
+        ELSE 'C'
+        END AS GRADE
+    FROM HR_EMPLOYEES A
+    JOIN HR_GRADE B
+    ON A.EMP_NO = B.EMP_NO
+    GROUP BY EMP_NO, EMP_NAME) AS B
+ON A.EMP_NO = B.EMP_NO
+ORDER BY EMP_NO

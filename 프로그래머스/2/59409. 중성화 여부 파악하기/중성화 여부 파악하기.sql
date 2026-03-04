5700cc2006cde8646aa7086f0ee78c7f0ca437c8
junseo Lee <74139368+juns0720@users.noreply.github.com>
2026-03-04 19:40:27 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,9 @@
+SELECT
+    ANIMAL_ID,
+    NAME,
+    CASE
+        WHEN SUBSTRING_INDEX(SEX_UPON_INTAKE,' ', 1) IN ('Neutered', 'Spayed') THEN 'O'
+        ELSE 'X'
+    END AS '중성화'
+FROM ANIMAL_INS
+    

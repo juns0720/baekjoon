fb2f5f9dfe466e759c396c7c601aca1fac4a0887
junseo Lee <74139368+juns0720@users.noreply.github.com>
2026-03-05 15:59:57 +0900

    [Gold III] Title: 트리 부수기, Time: 412 ms, Memory: 147072 KB -BaekjoonHub

--- 백준/Gold/32985. 트리 부수기/트리 부수기.py
+++ 백준/Gold/32985. 트리 부수기/트리 부수기.py
@@ -1,5 +1,5 @@
 import sys
-sys.setrecursionlimit(10**6)
+from collections import deque
 input = sys.stdin.readline
 
 N = int(input())
@@ -10,20 +10,27 @@ for i in range(N-1):
     graph[v1].append(v2)
     graph[v2].append(v1)
 
-def dfs(v1, depth):
-    res[v1] -= 1*depth
+def bfs():
 
-    for v2 in graph[v1]:
-        if visited[v2]:
-            continue
+    while queue:
+        v1, depth = queue.popleft()
+        res[v1] -= 1*depth
 
-        visited[v2] = 1
-        dfs(v2,depth + 1)
+        for v2 in graph[v1]:
+            if visited[v2]:
+                continue
 
+            visited[v2] = 1
+            queue.append((v2,depth+1))
+            
+
+queue = deque([(0,0)])
 res = [N-1 for _ in range(N)]
+
 visited = [0 for _ in range(N)]
 visited[0] = 1
-dfs(0,0)
+
+bfs()
 
 for i in list(reversed(res[1:])):
     print(1 if i % 2 == 1 else 0, end = '')
